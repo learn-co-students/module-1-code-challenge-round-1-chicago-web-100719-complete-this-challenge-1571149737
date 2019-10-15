3ed4b64140222235f049892cf82c36d1395a3854
benjewman <bennyadave@gmail.com>
2019-10-15 10:47:43 -0500

    final for the challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,39 @@
 class Review
-  
+    attr_reader :customer, :restaurant
+    attr_accessor :content
+    attr_writer :rating
+
+    @@all = []
+
+    def initialize(customer, restaurant, content, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @content = content
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rating
+        if @rating > 5
+            return 5
+        elsif @rating < 1
+            return 1
+        else return @rating
+        end
+    end
+
+    # def rating=(num)
+    #     if num > 5
+    #         @rating = 5
+    #     elsif num < 1
+    #         @rating = 1
+    #     else
+    #         @rating = num
+    #     end
+    # end
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+ben = Customer.new("Ben", "Davis")
+hope = Customer.new("Hope", "Ironmonger")
+anna = Customer.new("Anna", "Davis")
+benjy = Customer.new("Ben", "Gold")
+
+pequods = Restaurant.new("Pequods")
+lou = Restaurant.new("Lou Malnatis")
+ginos = Restaurant.new("Gino's")
+
+ben.add_review(pequods, "Awesome Sauce Possum Dawg!", 10)
+anna.add_review(lou, "sucks", -1)
+hope.add_review(pequods, "rocks socks!", 3)
+ben.add_review(ginos, "might be very very very good", 4)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
