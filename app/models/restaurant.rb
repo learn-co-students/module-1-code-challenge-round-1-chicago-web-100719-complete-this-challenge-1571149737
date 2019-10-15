e3f87d2504a033be6c15dcc86e3281a81fdd4fb3
bosmanpa <bsmn.ptrck@gmail.com>
2019-10-15 10:47:40 -0500

    code challenge 1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
   attr_reader :name
-
-  def initialize(name)
+  @@all = []
+  
+def initialize(name)
     @name = name
+    self.class.all << self
   end
+
+def self.all
+  @@all
+end
+
+def reviews
+  Review.all.select {|x| x.restaurant == self}
+end
+
+def customers
+  reviews.collect {|x| x.customer}.uniq
+end
+
+def average_star_rating
+  values = reviews.collect {|x| x.rating}
+  values.sum / values.size
+end
+
+def longest_review
+  allcontent = reviews.collect {|x| x.content}
+  allcontent.sort_by {|x| x.length}[-1]
+end
+
+def self.find_by_name(name)
+  self.all.find {|x| x.name == name}
+
+end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,22 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+joe = Customer.new("joe", "schmo")
+john = Customer.new("john", "doe")
+jane = Customer.new("jane", "james")
+joe2 = Customer.new("joe", "schmell")
+
+
+alinea = Restaurant.new("alinea")
+giant = Restaurant.new("giant")
+
+joealinea = Review.new(joe, alinea, "Good", 3)
+joegiant = Review.new(joe, giant, "Bad", 1)
+johnalinea = Review.new(john, alinea, "Very Good", 4)
+johngiant = Review.new(john, giant, "BEST RESTAURANT EVER", 5)
+jane.add_review(alinea, "Just Okay", 2)
+joe.add_review(giant, "Better", 3)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
