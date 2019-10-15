9de8611e5169243c737bd0597646960e431fdab8
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-10-15 10:47:54 -0500

    finished code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|r| r.restaurant == self}
+  end
+
+  def customers
+    all_cust = reviews.collect {|r| r.customer}
+    all_cust.uniq
+  end
+
+  def average_star_rating
+    ratings = reviews.collect {|r| r.rating}
+    ratings.sum / ratings.length
+  end
+
+  def longest_review
+    long_review = self.reviews.max {|a, b| a.content.length <=> b.content.length}
+    return long_review.content
+    #could also do a reverse sort_by and return the first item, but time is short and this is faster!
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|r| r.name == name}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,29 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+sarah = Customer.new("Sarah", "Laloggia")
+john = Customer.new("John", "Smith")
+alisha = Customer.new("Alisha", "Workman")
+sarahb = Customer.new("Sarah", "Blum")
+
+agami = Restaurant.new("Agami")
+oshaugn = Restaurant.new("O'Shaugnessy's")
+spoken = Restaurant.new("Spoken Cafe")
+spoken_2 = Restaurant.new("Spoken Cafe")
+
+sarah.add_review(oshaugn, "This is my favorite place!", 5)
+sarah.add_review(spoken, "best coffee in the neighborhood", 4)
+alisha.add_review(oshaugn, "Great food and beer", 4)
+john.add_review(agami, "good sushi, high prices", 3)
+john.add_review(agami, "went back, and they had added a great lunch special! More stuff so that this is definitely the longest review.", 4)
+john.add_review(spoken, "yummmm bagels", 5)
+sarah.add_review(agami, "good tuna, crab was just okay", 3)
+
+test_review1 = Review.new(sarahb, spoken_2, "Just as good as the original", 4)
+test_review2 = Review.new(alisha, spoken, "droooooooool", 4)
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
